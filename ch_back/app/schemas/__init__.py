ddb4128adc0b36207867f27bb0429d4875583ab2
haniyfdev <haniyfmuhsin@gmail.com>
2026-02-25 15:49:54 +0500

    FIX: Reorganized structure from root directory

--- ch_back/app/schemas/__init__.py
+++
@@ -1,5 +0,0 @@
-from .user import UserCreate, UserLogin, UserResponse, UserUpdate, UserChangePassword, AvatarResponse, TokenData, TokenResponse
-
-__all__ = [
-    "UserCreate", "UserLogin", "UserResponse", "UserUpdate", "UserChangePassword", "AvatarResponse", TokenData, TokenResponse
-]
