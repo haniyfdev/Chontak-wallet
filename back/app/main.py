194fa9615fa99e76eba5682f127d9cf98ba13119
haniyfdev <haniyfmuhsin@gmail.com>
2026-03-10 11:08:40 +0500

    .env added

--- back/app/main.py
+++ back/app/main.py
@@ -23,12 +23,11 @@ async def shutdown_event():
 # ----------------------
 app.add_middleware(
     CORSMiddleware,
-#     allow_origins = [
-#     "https://chontak.onrender.com",
-#     "http://localhost:5173",         
-#     "http://127.0.0.1:5173",
-# ],
-    allow_origins = ["*"],
+    allow_origins = [
+    "https://chontak.onrender.com",
+    "http://localhost:5173",         
+    "http://127.0.0.1:5173",
+],
     allow_credentials = True,
     allow_methods = ["*"],
     allow_headers = ["*"]
